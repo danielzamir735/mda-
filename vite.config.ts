f7d2984a4e7362b9439efc06be791516167544a9
Daniel Zamir <danielzamir735@github.com>
2026-03-06 14:55:51 +0200

    fix: PWA auto-update and CPR timer active

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,8 @@ export default defineConfig({
     VitePWA({
       registerType: 'autoUpdate',
       workbox: {
+        clientsClaim: true,
+        skipWaiting: true,
         globPatterns: ['**/*.{js,css,html,ico,png,svg,woff2}'],
         runtimeCaching: [
           {
